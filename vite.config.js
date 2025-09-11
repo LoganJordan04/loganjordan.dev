77599d5f51b0ab23b9c21a5b417a63f3fa996140
Logan Jordan <ljordan@cocc.edu>
2025-09-10 19:00:05 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
     plugins: [glsl(), cloudflare()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['gsap', 'three'],
+                    animations: ['@lottiefiles/dotlottie-web']
+                }
+            }
+        },
+        target: 'es2020',
+        minify: 'esbuild',
+        sourcemap: false
+    },
+    optimizeDeps: {
+        include: ['gsap', 'three', '@lottiefiles/dotlottie-web']
+    }
 });
